74a8708ee46df212d9f8599d52d30ad5efff50be
Abhishek Kumar <abhishek.kumar@niyuj.com>
2019-10-08 14:24:02 +0530

    add

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ function onListening() {
     : 'port ' + addr.port;
   ('Listening on ' + bind);
   logger.info('server listening on port' + addr.port, 'serverOnListeningHandler', 10);
-  let db = mongoose.connect(appConfig.db.uri,{ useNewUrlParser:true ,useCreateIndex:true});
+  let db = mongoose.connect('mongodb+srv://node-shop:node-user123@node-rest-shp-ift2f.mongodb.net/pharmaApp?retryWrites=true&w=majority',{ useNewUrlParser:true ,useCreateIndex:true});
 }
 
 process.on('unhandledRejection', (reason, p) => {
